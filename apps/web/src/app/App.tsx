ee8ee652dd07d400282ce645f4639df0ecafec34
Rhuan Lucas <rhuanlucassbs@gmail.com>
2026-01-22 09:59:59 -0300

    merge: feature/home-page-layout into develop/frontend (#1)
    Implementação inicial da interface da Home Page e estruturação da camada de serviços. Este merge consolida a base visual do projeto com TailwindCSS e prepara o fluxo de dados para futura integração com o backend PHP.

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,7 +1,50 @@
-import styles from './App.module.css'
+import {useEffect, useState} from 'react'
+import { Header } from '../components/layout/Header'
+import { HeroSearch } from '../components/ui/HeroSearch'
+import { TripCard } from '../components/ui/TripCard'
+import { tripService } from '../services/api'
+import type { Trip } from '../services/types'
+import '../styles/global.css'
 
 function App() {
-  return <div className={styles.app}>App</div>
+  const [trips, setTrips] = useState<Trip[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    tripService.getPopularTrips().then((response) => {
+      setTrips(response.data)
+      setLoading(false)
+    })
+  }, [])
+  return (
+    <div className="min-h-screen bg-white">
+      {/* Componente de Layout */}
+      <Header />
+      <main>
+        {/* Banner Azul e Barra de busca */}
+        <HeroSearch />
+
+        {/* Secao de listagem */}
+        <section className="max-w-7xl mx-auto px-6 mt-24 mb-12">
+          <div className="flex items-center gap-2 mb-8">
+            <span className="text-xl"></span>
+            <h2 className="text-2xl font-bold text-slate-800">Populares</h2>
+          </div>
+          {loading ? (
+            <div className="flex justify-center py-10">
+              <p className="text-gray-500 animate-pulse">Carregando viagens...</p>
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+               {trips.map((trip) => (
+                <TripCard key={trip.id} trip = {trip}/>
+               ))}
+            </div>
+          )}
+        </section>
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ apps/web/src/components/layout/Header/Header.tsx
@@ -0,0 +1,39 @@
+import { UserCircle, Globe, Tag, LifeBuoy } from 'lucide-react';
+
+export const Header = () => {
+  return (
+    <header className="w-full bg-white py-4 px-6 md:px-12 flex items-center justify-between border-b border-gray-100">
+      {/* Logo - Ajuste o caminho da imagem conforme seu projeto */}
+      <div className="flex items-center">
+        <div className="bg-blue-900 p-2 rounded-tl-[40px] rounded-br-[40px] h-28 w-28">
+           <img src="../public/logo.svg" alt="Goyta-Bus" className="h-22.5 w-22.5" />
+        </div>
+      </div>
+
+      {/* Navegação Central */}
+      <nav className="hidden md:flex items-center gap-8 text-gray-600 font-medium">
+        <a href="#" className="flex items-center gap-2 text-blue-700 border-b-2 border-blue-700 pb-1">
+          <Globe size={20} /> Viagens
+        </a>
+        <a href="#" className="flex items-center gap-2 hover:text-blue-700 transition-colors">
+          <Tag size={20} /> Ofertas
+        </a>
+        <a href="#" className="flex items-center gap-2 hover:text-blue-700 transition-colors">
+          <LifeBuoy size={20} /> Ajuda
+        </a>
+      </nav>
+
+      {/* Lado Direito: Login/Registro */}
+      <div className="flex items-center gap-4">
+        <button className="flex items-center gap-2 text-gray-700 hover:text-blue-700 font-medium">
+          <UserCircle size={24} className="text-gray-400" />
+          <span>Entrar</span>
+        </button>
+        <div className="h-6 w-[px] bg-gray-300 mx-1"></div>
+        <button className="text-gray-700 hover:text-blue-700 font-medium">
+          Registrar-se
+        </button>
+      </div>
+    </header>
+  );
+};
